105e8c7421200ce55a7a2d53eb5898e82e083871
Encapsulateed <alexey.mitroshkin@gmail.com>
2024-05-21 10:22:09 +0300

    Первая отработка сдана

---
+++ lab7/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Numerics;
+
+public class FftFunction
+{
+    private readonly Complex[] coefs;
+
+    public FftFunction(Complex[] coefs)
+    {
+        this.coefs = coefs;
+    }
+
+    public Complex CalсAt(double x)
+    {
+        Complex res = Complex.Zero;
+
+        for (int q = 0; q < coefs.Length; q++)
+        {
+            res += coefs[q] * Complex.Exp(-2 * Math.PI * Complex.ImaginaryOne * q * x);
+        }
+
+        return res;
+    }
+}
+
+public class Program
+{
+    private  static int r = 7;
+    private static int N = Convert.ToInt32(Math.Pow(2,r));
+
+    private static double Func(double x)
+    {
+        return Math.Sin(Math.Abs(x - 0.5));
+    }
+
+    private static FftFunction FFT((double, double)[] points)
+    {
+        Complex[] Aq = new Complex[N];
+
+        for (int q = 0; q < N; q++)
+        {
+            Complex[] APrev = new Complex[points.Length];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                APrev[i] = points[i].Item2;
+            }
+
+            for (int m = 1; m <= r; m++)
+            {
+                Complex[] A = new Complex[APrev.Length / 2];
+
+                for (int k = 0; k < APrev.Length / 2; k++)
+                {
+                    A[k] = 0.5 * (
+                        Complex.Exp(-2 * Math.PI * Complex.ImaginaryOne * 0 * (1.0 / Math.Pow(2, m)) * (q % (int)Math.Pow(2, m))) * APrev[k] +
+                        Complex.Exp(-2 * Math.PI * Complex.ImaginaryOne * 1 * (1.0 / Math.Pow(2, m)) * (q % (int)Math.Pow(2, m))) * APrev[k + APrev.Length / 2]);
+                }
+
+                APrev = A;
+            }
+
+            Aq[q] = APrev[0];
+        }
+
+        return new FftFunction(Aq);
+    }
+
+    public static void Main()
+    {
+        (double, double)[] points = new (double, double)[N];
+        for (int j = 0; j < N; j++)
+        {
+            points[j] = (j / (double)N, Func(j / (double)N));
+        }
+
+        FftFunction fft = FFT(points);
+
+        foreach ((double xj, _) in points)
+        {
+            var abs = Math.Abs(fft.CalсAt(xj).Real - Func(xj + 0.5 / N));
+            Console.WriteLine($"{xj + 0.5 / N} | {fft.CalсAt(xj).Real:F15} | {Func(xj + 0.5 / N):F15} | {abs}:F15");
+        }
+
+
+    }
+}
---
+++ lab8/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    static double F(double x)
+    {
+        return Math.Exp(x);
+    }
+
+    static double Analytical(double x)
+    {
+        return Math.Exp(x);
+    }
+
+    static int n = 10;
+    static double p = 1.0;
+    static double q = -1.0;
+    static double a = Analytical(0); // 1.0
+    static double b = Analytical(1); // Math.E
+    static double[][] ys = new double[2][];
+
+    static double GetC1()
+    {
+        return (b - ys[0][n]) / ys[1][n];
+    }
+
+    static double GetYi(int i)
+    {
+        return ys[0][i] + GetC1() * ys[1][i];
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("МЕТОД СТРЕЛЬБЫ");
+        Console.WriteLine($"y'' + {p}y' + {q}y = exp(x)");
+        Console.WriteLine($"y(0) = {a}\ny(1) = {b}");
+        Console.WriteLine($"Количество разбиений: {n}");
+        double h = 1.0 / n;
+        Console.WriteLine(h);
+        double delta = h * 100;
+        double[] xs = new double[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            xs[i] = i * h;
+        }
+        ys[0] = new double[n + 1];
+        ys[1] = new double[n + 1];
+        ys[0][0] = a;
+        ys[0][1] = a + delta;
+        ys[1][0] = 0;
+        ys[1][1] = delta;
+
+        for (int i = 1; i < n; i++)
+        {
+            ys[0][i + 1] = (h * h * F(xs[i]) + (2.0 - q * h * h) * ys[0][i] - (1.0 - h / 2 * p) * ys[0][i - 1]) / (1 + h / 2 * p);
+            ys[1][i + 1] = ((2.0 - q * h * h) * ys[1][i] - (1.0 - h / 2 * p) * ys[1][i - 1]) / (1 + h / 2 * p);
+        }
+
+        double[] y = new double[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            y[i] = GetYi(i);
+        }
+        Console.WriteLine(y.Length);
+        for (int i = 0; i < y.Length; i++)
+        {
+            Console.WriteLine($"x={xs[i]:F1}, y={Analytical(xs[i]):F6}, y*={y[i]:F6}  |y-y*|={Math.Abs(y[i] - Analytical(xs[i])):F6}");
+        }
+    }
+}
